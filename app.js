95a10be938b88efda9221b7d876158651bf3c5c8
gopal verma <vermagopal1625@gmail.com>
2025-07-12 21:27:04 +0530

    fix: use dynamic port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import axios from "axios";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 // Middleware
 app.use(express.static("public"));
